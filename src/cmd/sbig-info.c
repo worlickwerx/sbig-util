feb31ca8d5ce8fc7774cdb5568b06df42eb56831
Demo User <debian@beaglebone.(none)>
2014-09-19 15:09:09 +0000

    sbig-info: improve command structure

--- src/cmd/sbig-info.c
+++ src/cmd/sbig-info.c
@@ -26,25 +26,55 @@
 #include <stdio.h>
 #include <libgen.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <errno.h>
 #include <string.h>
 #include <stdlib.h>
 #include <dlfcn.h>
+#include <getopt.h>
 
 #include "src/common/libsbig/sbig.h"
 #include "src/common/libutil/log.h"
 
-void show_driver_info (sbig_t sb);
-void show_device_info (sbig_t sb);
+void show_driver_info (sbig_t sb, int ac, char **av);
+void show_device_info (sbig_t sb, int ac, char **av);
+
+#define OPTIONS "h"
+static const struct option longopts[] = {
+    {"help",          no_argument,           0, 'h'},
+    {0, 0, 0, 0},
+};
+
+void usage (void)
+{
+    fprintf (stderr,
+"Usage: sbig-info driver\n"
+"       sbig-info device\n"
+);
+    exit (1);
+}
 
 int main (int argc, char *argv[])
 {
     sbig_t sb;
     const char *sbig_udrv = getenv ("SBIG_UDRV");
     int e;
+    int ch;
+    char *cmd;
 
     log_init ("sbig-info");
 
+    while ((ch = getopt_long (argc, argv, OPTIONS, longopts, NULL)) != -1) {
+        switch (ch) {
+            case 'h': /* --help */
+            default:
+                usage ();
+        }
+    }
+    if (optind == argc)
+        usage ();
+    cmd = argv[optind++];
+ 
     if (!sbig_udrv)
         msg_exit ("SBIG_UDRV is not set");
     if (!(sb = sbig_new ()))
@@ -54,21 +84,25 @@ int main (int argc, char *argv[])
     if ((e = sbig_open_driver (sb)) != 0)
         msg_exit ("sbig_open_driver: %s", sbig_strerror (e));
 
-
-    show_driver_info (sb);
-    //show_device_info (sb);
+    if (!strcmp (cmd, "device"))
+        show_device_info (sb, argc - optind, argv + optind);
+    else if (!strcmp (cmd, "driver"))
+        show_driver_info (sb, argc - optind, argv + optind);
 
     sbig_destroy (sb);
     log_fini ();
     return 0;
 }
 
-void show_driver_info (sbig_t sb)
+void show_driver_info (sbig_t sb, int ac, char **av)
 {
     ushort version;
     ushort maxreq;
     char *name;
     int e;
+
+    if (ac != 0)
+        msg_exit ("driver takes no arguments");
 
     if ((e = sbig_get_driver_info (sb, &version, &name, &maxreq)) != 0)
         msg_exit ("sbig_get_driver_info: %s", sbig_strerror (e));
@@ -78,10 +112,12 @@ void show_driver_info (sbig_t sb)
     free (name);
 }
 
-void show_device_info (sbig_t sb)
+void show_device_info (sbig_t sb, int ac, char **av)
 {
     int e;
 
+    if (ac != 0)
+        msg_exit ("device takes no arguments");
     if ((e = sbig_open_device (sb)) != 0)
         msg_exit ("sbig_open_device: %s", sbig_strerror (e));
     if ((e = sbig_close_device (sb)) != 0)
